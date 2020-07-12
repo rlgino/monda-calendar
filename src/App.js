5df384236049d904d45360714d7f9a9c44b62f02
Gino Luraschi <gino.luraschi@mercadolibre.com>
2020-07-11 23:06:53 -0300

    Add style

--- src/App.js
+++ src/App.js
@@ -1,55 +1,75 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
+  const MESES = {
+    1: "Enero",
+    2: "Febrero",
+    3: "Marzo",
+    4: "Abril",
+    5: "Mayo",
+    6: "Junio",
+    7: "Julio",
+    8: "Agosto",
+    9: "Septiembre",
+    10: "Octubre",
+    11: "Noviembre",
+    12: "Diciembre"
+  }
+  const [date, setDate] = useState(new Date());
+  
+  const [firstDays, setFirstDays] = useState([])
+  const [daysOfMonth, setDaysOfMonth] = useState([])
   useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
+    
+    var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    console.log(lastDay);
+    let list = [];
+    for (var i = 1; i <= lastDay; i++) {
+      list.push(i);
     }
-    getDate();
-  }, []);
+    setDaysOfMonth(list);
+
+    var firstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+    console.log(firstDay);
+    let firstDays = [];
+    for (var i = 1; i <= firstDay; i++) {
+      firstDays.push(i);
+    }
+    setFirstDays(firstDays)
+
+    return () => { }
+  }, [])
   return (
     <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
+      <h1>{date.getFullYear()}</h1>
+
+      <h2>{MESES[date.getMonth()]}</h2>
+      <h3>{date.getDate()}</h3>
+      <div className="container">
+        <div className="day-header">Domingo</div>
+        <div className="day-header">Lunes</div>
+        <div className="day-header">Martes</div>
+        <div className="day-header">Miercoles</div>
+        <div className="day-header">Jueves</div>
+        <div className="day-header">Viernes</div>
+        <div className="day-header">Sábado</div>
+        {
+          firstDays.map(d => {
+            return(
+              <div className="day">X</div>
+            )
+          })
+        }
+        {daysOfMonth.map(day => {
+          return(
+            <div className="day">
+              <div className={day === date.getDate() ? "day-selected" : ""}>{day}</div>
+            </div>
+          )
+        })}
+      </div>
     </main>
   );
 }
